6a8d6983a572a9f888df793359dfd5d0725c0436
Dan <decisivedaniel@proton.me>
2024-10-27 19:30:57 +0100

    Move body of issue into issueDetail

--- src/RepoIssues/IssueDetail.tsx
+++ src/RepoIssues/IssueDetail.tsx
@@ -1,17 +1,20 @@
 import React, {useState} from 'react';
 import { useLocation, Link, useNavigate } from 'react-router-dom';
 import { issueResponse } from '../InfoRetrival/gitService';
+import Markdown from 'react-markdown';
 
 export interface IssueState {
     issue:issueResponse;
 }
 
 export function IssueDetail() {
-    const location:issueResponse = useLocation().state;
+    const issue:issueResponse = useLocation().state;
     const navigate = useNavigate();
     return (
         <div>
-            <p>{location.body}</p>
+            <h1>{issue.title}</h1>
+            {issue.assignee !== undefined ? <h2>Assigned to {issue.assignee?.name}</h2> : null}
+            <Markdown>{issue.body}</Markdown>
             <Link to={{pathname: "/"}}>return</Link>
             <button onClick={()=> navigate(-1)}>Nav Return</button>
         </div>
--- src/RepoIssues/IssueOverview.tsx
+++ src/RepoIssues/IssueOverview.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Markdown from 'react-markdown';
 import { issueResponse } from '../InfoRetrival/gitService';
 import { Link } from 'react-router-dom';
 
@@ -8,11 +7,9 @@ interface IssueOverviewProps {
 }
 
 export default function IssueOverview ({issue}:Readonly<IssueOverviewProps>) {
-
     return (
         <div>
             <h1>{issue.number}: {issue.title}</h1>
-            <Markdown>{issue.body}</Markdown>
             <Link to={{pathname: "/issue"}} state={issue} >Navigate to Issue</Link>
         </div>
     )
